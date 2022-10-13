d07ef307c1573e1ee34b1ebe2480a2c8a1e15e09
renlin-code <renelj1997@gmail.com>
2022-10-13 12:20:25 +0500

    Added file json with certificates and rendering using forEach

--- js/main.js
+++ js/main.js
@@ -1,7 +1,61 @@
-const initFlickity = () => {
-    const flkty = new Flickity( ".certificates__slider", {
-      pageDots: false,
-      autoPlay: true
-    });
+//NAVIGATOR MENU
+const burgerDOM = document.querySelector("#burger-checkbox");
+const navMenuDOM = document.querySelector(".header__nav");
+const bodyDOM = document.querySelector("body");
+
+
+
+burgerDOM.addEventListener("click", () => {
+  navMenuDOM.classList.toggle("header__nav--open");
+  bodyDOM.classList.toggle("fixed");
+})
+
+const menuItemClick = () => {
+  burgerDOM.checked = false
+  navMenuDOM.classList.remove("header__nav--open");
+  bodyDOM.classList.remove("fixed");
+};
+
+
+
+
+//CERTIFICATES
+const renderCertificates = async () => {
+  const url = "../certificates.json";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const sliderDOM = document.querySelector(".certificates__slider");    
+
+  data.forEach(item => {
+    const slideDOM = document.createElement("div");
+    const slideFrontLayerDOM = document.createElement("div");
+    const certificateDOM = document.createElement("img");
+
+    slideDOM.classList.add("certificates__slider-slide");
+    slideFrontLayerDOM.classList.add("front-layer")
+    certificateDOM.src = item.url_mini;
+
+    slideDOM.appendChild(slideFrontLayerDOM);
+    slideDOM.appendChild(certificateDOM);
+
+    sliderDOM.appendChild(slideDOM);
+
+    // certificate.addEventListener("click", () => {
+    //     popupView(i);
+    // })
+
+    // certificateContainer.appendChild(certificate);
+    // certificatesSlider.appendChild(certificateContainer);
+
+    
+  });
+  const flkty = new Flickity( ".certificates__slider", {
+    pageDots: false,
+    autoPlay: true
+  });
 }
-document.addEventListener("DOMContentLoaded", initFlickity);
+
+renderCertificates();
+
+
